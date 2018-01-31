197000c5989e9dc1a44e89f1dc9aaa2f0730a845
Milena Araujo <milena.carvalho@viasoft.com.br>
2018-01-31 09:22:42 -0200

    Alterado tamanho do editor de texto, retirado botão de fullscreen e configurada as teclas de tabspace, enter e espaço.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,14 +8,31 @@ import { Component, OnInit, OnDestroy, ElementRef, ViewChild } from '@angular/co
 export class HomeComponent implements OnInit  {
   
   public texto: string = '';
+  public options: Object = {
+    placeholderText: 'Digite aqui',
+    height: 279,
+    toolbarButtons: ['bold', 'italic', 'underline', 'strikeThrough', '|', 'fontFamily',
+        'fontSize', 'color', '|', 'paragraphFormat', 'align', 'formatOL', 'formatUL', 
+        'outdent', 'indent', 'quote', '-', 'insertLink', 'insertImage', 'embedly', 
+        'insertFile', 'insertTable', '|', 'emoticons', 'specialCharacters', 'selectAll', 
+        'clearFormatting', '|', 'print', 'spellChecker', 'help', 'html', '|', 'undo', 'redo']
+  };
 
   constructor(){ }
 
   ngOnInit() {}
 
-  public getValue(keyValue: any){
-    this.texto += keyValue; 
-    console.log(this.texto)
+  public getValue(keyValue: any) {
+    if(keyValue === ' '){
+      keyValue = '&nbsp;';
+    } else if ( keyValue === '\n'){
+      keyValue = '<br>';
+    } else if ( keyValue === '\t'){
+      keyValue = '&nbsp;&nbsp;&nbsp;&nbsp;';
+    }
+    this.texto += keyValue; // texto que vai no editor
+    console.log(keyValue);
+    console.log(this.texto);
   }
 
 }
