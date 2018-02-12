ad8ffd5e7142e8d273919b0f249e5949140b5a93
Gustavo Barros <gustavo.barros@viasoft.com.br>
2018-02-12 17:14:39 -0200

    Ajustes na inserção de usuários no banco de dados

--- src/apis/register.api.ts
+++ src/apis/register.api.ts
@@ -12,35 +12,67 @@ export class Register extends BaseRoute {
 
     public registerUser(req: Request, res: Response, next: NextFunction){
         console.log("REGISTRO");
-        if(this.checkIfAlreadyExists(req, res)){
-            console.log("Esse email já foi cadastrado!");
-            res.status(400).json({message: 'Esse email já foi cadastrado!'});
-        } else {
-            this.insertIntoDatabase(req, res);
-            console.log("Usuário cadastrado com sucesso!");
-            res.status(200).json({message: 'Registro feito com sucesso.'});
-            // send confirmation email?
-        }
+        
+        res.locals.mongoAccess.coll[0].find({"email": req.body['email']}).toArray(function(err, user_list) {         
+            if(user_list.length !== 0){
+                console.log("USER FOUND!");
+                console.log(user_list);
+                console.log("Esse email já foi cadastrado!");
+                res.status(400).json({message: 'Esse email já foi cadastrado!'});
+                return true;
+            } else {
+                console.log("USER NOT FOUND!");
+                //this.insertIntoDatabase(req, res);
+
+                let user = new UserModel();
+                user.fullName = req.body['fullName'];
+                user.email = req.body['email'];
+                user.password = req.body['password'];
+        
+                // Remove caracteres indesejados do input do usuário    
+                mongoSanitize.sanitize(user.fullName);
+                mongoSanitize.sanitize(user.email);
+                mongoSanitize.sanitize(user.password);
+        
+                res.locals.mongoAccess.coll[0].insert(user, (err, result) => {
+                    //console.log(user);
+                    console.log("Usuário inserido na database!");
+                });  
+
+
+                console.log("Usuário cadastrado com sucesso!");
+                res.status(200).json({message: 'Registro feito com sucesso.'});
+                return false;
+            }
+    })
         
         
     }
 
-    private checkIfAlreadyExists(req: Request, res: Response): any {  
+    /*
+    public checkIfAlreadyExists(req: Request, res: Response): any {  
             res.locals.mongoAccess.coll[0].find({"email": req.body['email']}).toArray(function(err, user_list) {         
                     if(user_list.length !== 0){
                         console.log("USER FOUND!");
                         console.log(user_list);
+                        console.log("Esse email já foi cadastrado!");
+                        res.status(400).json({message: 'Esse email já foi cadastrado!'});
                         return true;
                     } else {
                         console.log("USER NOT FOUND!");
+                        this.insertIntoDatabase(req, res);
+                        console.log("Usuário cadastrado com sucesso!");
+                        res.status(200).json({message: 'Registro feito com sucesso.'});
                         return false;
                     }
             })
 
      }
 
+*/
+/*
 
-    private insertIntoDatabase(req: Request, res: Response){
+    public insertIntoDatabase(req: Request, res: Response){
         let user = new UserModel();
         user.fullName = req.body['fullName'];
         user.email = req.body['email'];
@@ -52,21 +84,11 @@ export class Register extends BaseRoute {
         mongoSanitize.sanitize(user.password);
 
         res.locals.mongoAccess.coll[0].insert(user, (err, result) => {
-            console.log(user);
+            //console.log(user);
             console.log("Usuário inserido na database!");
         });  
-        /*  
-        var mongodb = require('mongodb') , MongoClient = mongodb.MongoClient
-        const myAwesomeDB = '';
-        MongoClient.connect(process.env.MONGOHQ_URL|| 'mongodb://localhost:27017',
-             function(err, database) {  
-                var db = database.db('etm-database');
-                db.collection('keyboards').insert(user, (err, result) => {
-                    console.log("Keyboard inserido")
-            })
-        })
-        */
     }     
+*/
 
 
 }
