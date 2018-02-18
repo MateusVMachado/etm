2e6d4773f8d7813b5c56e1bced5e5c2f06bccf9a
Kewin Brand <kewin.robetti@viasoft.com.br>
2018-02-18 01:18:51 -0300

    Renomeado arquivo.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { HttpClientModule } from '@angular/common/http';
-import { CoreModule } from './core.module';
+import { CoreModule } from './nebular-core.module';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ThemeModule } from './theme.module';
