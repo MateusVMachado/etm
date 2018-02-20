3cdf945fd09b00f4a55b59bdc1ee3bce23b373c5
Gustavo Barros <gustavo.barros@viasoft.com.br>
2018-02-20 13:30:00 -0300

    Inserção de parâmetros na instanciação do ActionFactory

--- OpenFac.ActionFactory.ts
+++ OpenFac.ActionFactory.ts
@@ -2,20 +2,25 @@ import { IOpenFacAction } from './OpenFac.Action.Interface';
 export class OpenFacActionFactory {
 
     public static dicTypes: Map<string, any> = new Map<string, any>();
+    public static dicInsta: Map<string, any> = new Map<string, any>();
     
     public static Create(actionName: string, args?: any): IOpenFacAction {
         //primeiro faz o get no dic 
         let result = this.dicTypes.get(actionName)
         if( result ){
-            return new result(args);
+            let instance = this.dicInsta.get(actionName)
+            return new result(args, instance);
         } else {
             throw new console.error("No type registered for this id");
             
         }    
     }
 
-    public static Register<T extends IOpenFacAction>(actionName: string, type: { new(args?: any): T ;}) {
+    public static Register<T extends IOpenFacAction>(actionName: string, 
+                                                    type: { new(args?: any): T ;}, 
+                                                    instance: any) {
         OpenFacActionFactory.dicTypes.set(actionName, type);
+        OpenFacActionFactory.dicInsta.set(actionName, instance);
     }
 
    
