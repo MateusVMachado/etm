f7cf3ddb1f50a789fa3302b174345512dafa0b35
Kewin Brand <kewin.robetti@viasoft.com.br>
2018-03-01 15:27:09 -0300

    iniciada a correção do layout

--- src/app/components/editor-teclado/editor-teclado.component.ts
+++ src/app/components/editor-teclado/editor-teclado.component.ts
@@ -1,12 +1,19 @@
 import { Component, OnInit, Output, Input, EventEmitter } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
     selector: 'app-editor-component',
     templateUrl: './editor-teclado.component.html'
 })
-export class EditorTecladoComponent implements OnInit {     
+export class EditorTecladoComponent implements OnInit { 
+    public tamanho: number;    
     constructor() { }
 
-    ngOnInit() { }
+    ngOnInit() { 
+        console.log($("#EditorTecladoContainer").height());
+        console.log($("#teclado").height());
+        console.log("div: " + $("#editor").height());
+        this.tamanho = $("#editor").height();
+    }
 
 }
--- src/app/components/editor/editor.component.ts
+++ src/app/components/editor/editor.component.ts
@@ -1,5 +1,6 @@
-import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, ViewChild, AfterViewInit, Input } from '@angular/core';
 import { EditorTecladoService } from '../editor-teclado/editor-teclado.service';
+import * as $ from 'jquery';
 
 @Component({
     selector: 'app-editor',
@@ -7,20 +8,26 @@ import { EditorTecladoService } from '../editor-teclado/editor-teclado.service';
     styleUrls: ['./editor.component.css']
 })
 export class EditorComponent implements AfterViewInit {
-    @ViewChild('editor') editor: any;  
+    @ViewChild('editor') editor: any; 
+    @Input('tamanho') tamanhoDiv: number;
+   
     private editorConfig: any;
+    private height: number;
+
+    ngOnInit(){
+        let dif = ($(".editorContainer").height()) - this.tamanhoDiv;
+        console.log(this.tamanhoDiv)
+        let height: number = ($(".editorContainer").height() - dif);
+        console.log("editor: " + height)
+        this.editorConfig = { height: height};
+    }
     
     ngAfterViewInit(): void {
-        this.editor.ckeditorInit(this.editorConfig || {});
     }
 
     constructor(public editorTecladoService: EditorTecladoService){
-        this.editorConfig = { height: 170, language: 'pt-br'};
     }
 
-    config(){
-        this.editor.ckeditorInit(this.editorConfig || {});
-    }
 
     emitInstance($event){
         this.editorTecladoService.emitEditorInstance(this.editor.instance);
