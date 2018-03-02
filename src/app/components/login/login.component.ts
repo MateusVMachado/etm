25955cd6453a8013cdcc1c0e7740902148dd5d21
Gustavo Barros <gustavo.barros@viasoft.com.br>
2018-03-02 10:40:42 -0300

    ETM-12 Correção do bug do botão de entrar não ser ativado quando o auto-fill do browser está ligado

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -3,22 +3,28 @@ import { ConfigModel } from '../config/config';
 import { User } from '../shared/models/user';
 import { ConfigService } from '../config/config.service';
 import { AppBaseComponent } from '../shared/components/app-base.component';
-import { Component, Inject, ViewChild, OnInit, Injector } from '@angular/core';
+import { Component, Inject, ViewChild, OnInit, Injector, AfterContentChecked, AfterViewChecked, AfterViewInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../shared/services/auth.services';
 import { JWTtoken } from '../../storage';
 import 'rxjs/add/operator/catch';
 import { CookieService } from 'ngx-cookie-service';
+import { NgForm } from '@angular/forms';
 
 @Component({
     selector: 'nb-login',
     templateUrl: './login.component.html',
 })
 
-export class NgxLoginComponent extends AppBaseComponent {
+export class NgxLoginComponent extends AppBaseComponent implements OnInit, AfterViewInit  {
+    @ViewChild('form') form: NgForm;
     errors: string[] = [];
     messages: string[] = [];
-    user: any = {};
+    user: any = {
+      email: '',
+      password: '',
+      rememberMe: ''
+    };
     userInfo: any = {};
 
     constructor(protected service: AuthService,
@@ -28,6 +34,16 @@ export class NgxLoginComponent extends AppBaseComponent {
                 private profileService: ProfileService,
                 private configService: ConfigService
                 ) { super(injector)}
+
+    public ngAfterViewInit(){
+      // WORKAROUND PARA RESOLVER PROBLEMA DO BOTÃO DE ENTRAR 
+      // NÃO SER ATIVADO COM O AUTOFILL DO BROWSER
+      this.user.password = ''; 
+    }                
+
+    public ngOnInit(){
+
+    }
 
     public login(): void {
         let usuario: User = new User();
