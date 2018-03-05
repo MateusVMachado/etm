b21a94df4463cfd7e78ffff37aee570bd4a32f6f
Kewin Brand Robetti <kewin.robetti@viasoft.com.br>
2018-03-05 08:23:45 -0300

    Pasta /dist removida pois é gerada toda vez que o projeto é compilado.

--- dist/apis/mongoAcces.model.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class MongoAccessModel {
-    constructor() {
-        this.database = undefined;
-        this.coll = undefined;
-    }
-}
-exports.MongoAccessModel = MongoAccessModel;
