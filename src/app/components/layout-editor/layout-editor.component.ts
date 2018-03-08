ef0e9dbee3a23ffd852fb08c7700c4788ce3125c
Kewin Brand <kewin.robetti@viasoft.com.br>
2018-03-08 18:21:14 -0300

    ETM-38 Aplicação do Dragula para fazer o drag das teclas para o teclado Criação de um botão semelhante ao btn do bootstrap para conseguir fazer o Dragula funcionar

--- src/app/components/layout-editor/layout-editor.component.ts
+++ src/app/components/layout-editor/layout-editor.component.ts
@@ -20,14 +20,9 @@ export class LayoutEditorComponent implements OnInit, OnDestroy {
     public masterKeys: TecladoModel = new TecladoModel(); 
     public teclado: TecladoModel = new TecladoModel();
     private correctChoices: any;
+    public matrixIndex: string;
 
     constructor(private router: Router, private tecladoService: TecladoService, private dragulaService: DragulaService) {
-      //this.tecladoService.loadData().subscribe((data)=>{
-      //   console.log(data);
-         //this.tecladoService.convertLayoutToKeyboard(this.teclado, data[0]);
-      //   this.createEmptyKeyboard();
-      //});
-        //setup dragNdrop for creating content
         dragulaService.setOptions('fifth-bag', {
           copy: true,
           copySortSource: true
@@ -41,13 +36,32 @@ export class LayoutEditorComponent implements OnInit, OnDestroy {
     }
 
 
+    ngOnDestroy() {
+      //Called once, before the instance is destroyed.
+      //Add 'implements OnDestroy' to the class.
+      this.dragulaService.destroy('fifth-bag'); 
+    }
+
+    ngOnInit() {
+      //this.tecladoService.subscribeToTecladoSubject().subscribe((result) => {
+      //  this.teclado = result;
+      //});
+      
+    }
+
     private onDrop(value) {
       if (value[2] == null) //dragged outside any of the bags
           return;
-      if (value[2].id !== "content" && value[2].id !== value[3].id) //dragged to a container that should not add the element
+      if (value[2].id !== "content" && value[2].id !== value[3].id) { //dragged to a container that should not add the element
+          console.log(value[2].title );
           value[1].remove();
     
+        } else {
+          console.log(value[2].title);
+          //value[1].remove();
         }
+    }    
+
 
     private createEmptyKeyboard(){
       //let line = new Array();
@@ -67,19 +81,6 @@ export class LayoutEditorComponent implements OnInit, OnDestroy {
         this.masterKeys.teclas.push(line);  
     }   
       //this.teclado.teclas.push()
-    }
-
-    ngOnDestroy() {
-      //Called once, before the instance is destroyed.
-      //Add 'implements OnDestroy' to the class.
-      
-    }
-
-    ngOnInit() {
-      //this.tecladoService.subscribeToTecladoSubject().subscribe((result) => {
-      //  this.teclado = result;
-      //});
-      
     }
 
     private addLine(){
