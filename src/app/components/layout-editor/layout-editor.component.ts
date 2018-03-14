23fb4b789e6084913c934fd0b5e18c97cfd2e0a0
Kewin Brand <kewin.robetti@viasoft.com.br>
2018-03-14 17:04:47 -0300

    ETM-50 uso de Jquery para manipular elementos do DOM

--- src/app/components/layout-editor/layout-editor.component.ts
+++ src/app/components/layout-editor/layout-editor.component.ts
@@ -4,7 +4,7 @@ import { Router } from '@angular/router';
 import { TecladoComponent } from '../teclado/teclado.component';
 import { TecladoModel } from '../teclado/teclado.model';
 import { TecladoService } from '../teclado/teclado.service';
-import { DragulaService } from 'ng2-dragula';
+import { DragulaService, dragula } from 'ng2-dragula';
 import { OpenFACLayout, LayoutLine, LayoutButton } from './layout.model';
 import { AppBaseComponent } from '../shared/components/app-base.component';
 import { HttpClient } from '@angular/common/http';
@@ -14,6 +14,9 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { LayoutModalComponent } from './layout-modal/layout-modal.component';
 import { DeleteLayoutModalComponent } from './delete-layout/delete-layout-modal.component';
 import { Subscription } from 'rxjs';
+import { KeyboardNamesList } from '../sidebar/keyboards-list.model';
+
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-layout-editor',
@@ -39,6 +42,9 @@ export class LayoutEditorComponent extends AppBaseComponent implements OnInit, O
     private keyboardName: string;
     private layoutEditorServiceSubscribe: Subscription;
 
+    public keyboardToEdit: string;
+    public keyboardItems: KeyboardNamesList = new KeyboardNamesList();
+
     constructor(private router: Router, 
                 private tecladoService: TecladoService, 
                 private dragulaService: DragulaService,
@@ -47,8 +53,14 @@ export class LayoutEditorComponent extends AppBaseComponent implements OnInit, O
                 private http: HttpClient,
                 private layoutEditorService: LayoutEditorService,
                 private sidebarService: SideBarService,
-                private modalService: NgbModal) {
+                private modalService: NgbModal,
+                private sideBarService: SideBarService) {
       super(injector);
+
+      let user = this.authService.getLocalUser();
+      this.sideBarService.loadKeyboardsNames(user.email).subscribe((result) => {
+          this.keyboardItems = result;
+      });
 
       dragulaService.setOptions('master-bag', {
         accepts: function (el, target, source, sibling) {
@@ -88,7 +100,100 @@ export class LayoutEditorComponent extends AppBaseComponent implements OnInit, O
 
     ngOnInit() {
 
-      
+      var counter = 0;
+
+      var drake = dragula({});
+
+      /*
+      $('#add_section').on('click', function() {       
+        var elem = '<div class="main row"><div class="tamanho-button-especial"> <div></div>' + (counter++) + '</div></div>';       
+        $('#container').append(elem);
+        drake.containers.push($('.main').last().get(0));
+      })*/
+      let totalLength = 0;
+      //for(let i = 0 ; i < this.teclado.teclas.length; i++){
+      //  for(let j=0; j < this.teclado.teclas.length[i].length; j++){
+      //      totalLength += 1;
+      //  }
+     // }
+
+      this.tecladoReplicant.teclas.forEach(element => {
+        element.forEach(element => {
+            totalLength += 1;
+        });
+      });
+
+      $('#loadKeyboard').on('change', function() {       
+         /*
+          var elem = '<div style="background-color: rgb(94, 175, 221);'+
+          'color:black;'+
+          'height:100%;'+
+          'width: 100%;'+
+          'position: relative;'+
+          'top: 0;'+
+          'margin-right: 6px;'+
+          'border-radius:  8px;'+
+          'display: inline-block;'+
+          'text-align: center;'+
+          'white-space: nowrap;'+
+          'vertical-align: middle;'+
+          'user-select: none;'+
+          'border: 1 solid white;'+
+          'font-family: Exo;"><div>'+ (counter++) +'</div></div>';       
+          */
+          
+          
+          var elem2;
+          elem2 = $("[id=copy]")[1].cloneNode(true);
+          
+
+          let iCount = 0;
+          let jCount = 0;
+          $("[id=content]").each(function(index){
+            //console.log($(this).index())
+            //$(this).append(elem2);
+            //$(this).last().append(elem2);
+          })
+
+          
+          let sEl = $("[id=copy]").clone();
+
+          console.log(totalLength);
+          for(let i = 0; i< totalLength; i++){
+            if(!sEl[i]) break;
+            //sEl[i] é o elemento a ser copiado e $("[id=content]")[i] é o receptor 
+ 
+            //let el = $("[id=copy]")[1].cloneNode(true);
+            //let sEl = $("[id=copy]").clone();
+            //sEl[i].id = "newContent";
+            //sEl[1].setAttribute('style', 'background-color: rgb(94, 175, 221)');
+            //console.log(sEl[1].attributes);
+            sEl[i].removeAttribute('tooltip');
+            
+            //sEl[1].setAttribute('tooltip', '');
+            //$("#example").attr('title', obj.text);
+            let el = sEl[i].cloneNode(true);
+            //if($(el).find('input')[0].className) $(el).find('input')[0].className = 'tamanho-button-especial-full';
+            if(!$(el).find('input')[0]){
+              //console.log($(value[1]).find('button')[0].className);
+              $(el).find('button')[0].className = 'tamanho-button-especial-full'
+            }  else {
+              $(el).find('input')[0].className = 'tamanho-button-especial-full'
+           }
+
+
+            //console.log($(el).find('input')[0].className = 'tamanho-button-especial-full');
+            //console.log(el);
+            
+            $("[id=content]")[i].appendChild(el);
+            
+            //$("[id=content]")[i].setAttribute('style', 'background-color: blue');
+          }
+ 
+
+      })
+
+      //drake.containers.push($('.main').get(0));
     }
 
 
@@ -145,7 +250,9 @@ export class LayoutEditorComponent extends AppBaseComponent implements OnInit, O
 
           return;
       }    
+          
           let newTitle = value[2].className.split('@');
+          
           let title = newTitle[1];
 
 
@@ -154,12 +261,32 @@ export class LayoutEditorComponent extends AppBaseComponent implements OnInit, O
           let x = <number>parts[0];
           let y = <number>parts[1];
 
+          //console.log(value[2]);
+          //console.log($(value[1]).find('input').val() );
+          console.log($(value[1]).find('button')[0]);
 
-         
+          if($(value[1]).find('input')[0] !== undefined){
+            console.log("MARK1");
+            //console.log($(value[1]).find('button')[0].className);
+            $(value[1]).find('button')[0].className = 'tamanho-button-especial-full'
+            console.log("MARK2");
+          }  else if ($(value[1]).find('input')[0] !== undefined) {
+            console.log("MARK3");
+            $(value[1]).find('input')[0].className = 'tamanho-button-especial-full'
+            console.log("MARK4");
+         } else {
+          console.log("MARK5");
           value[1].className = "tamanho-button-especial-full";
+          console.log("MARK6");
+         }
+          //console.log($(value[1]).find('input')[0].className = 'tamanho-button-especial-full');
+          
+          console.log(value[1]); 
+          //value[1].className = "tamanho-button-especial-full";
 
-
+          console.log("MARK7");
           if(this.tecladoReplicant.teclas[y][x] === "") this.tecladoReplicant.teclas[y][x] = value[1].value ;
+          console.log("MARK8");
           console.log(JSON.stringify(this.tecladoReplicant) );
 
 
@@ -170,7 +297,8 @@ export class LayoutEditorComponent extends AppBaseComponent implements OnInit, O
     }
 
     public sayHello(mystring: string){
-  
+      console.log(this.keyboardToEdit);
+
     }
 
     public populateLayout(replicant: TecladoModel, email: string): OpenFACLayout{
