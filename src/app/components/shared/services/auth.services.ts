8d4b040cbbc1abac65e0b13da7fb2947edda773f
Kewin Brand <kewin.robetti@viasoft.com.br>
2018-04-05 16:46:29 -0300

    ETM-65 -Remoção de função

--- src/app/components/shared/services/auth.services.ts
+++ src/app/components/shared/services/auth.services.ts
@@ -72,10 +72,6 @@ export class AuthService extends AppServiceBase {
         return this.user;
     }
 
-    public getUserCookie(){
-
-    }
-
     public getUser(email:string) {
         return this.http.get(this.backendAddress + `/user?email=${email}`);
     }
