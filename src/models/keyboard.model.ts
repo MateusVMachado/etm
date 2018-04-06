047e054521be3eb93a3a2eca8d5e5a0f341481d9
Kewin Brand <kewin.robetti@viasoft.com.br>
2018-04-06 15:55:13 -0300

    ETM-71 +Adição de campo no keyboard.model.ts

--- src/models/keyboard.model.ts
+++ src/models/keyboard.model.ts
@@ -7,12 +7,14 @@ export class KeyboardModel {
     //userId: string;
     teclas: string[][];
     text: string[][];
+    action: string[][];
     type: string;
 
     constructor() {
         //this.userId = undefined;
         this.teclas = [];
         this.text = [];
+        this.action = [];
         this.type = 'none';
     }
 }
