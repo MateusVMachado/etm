89aae4e230bd01a5e58f7ef3f0e001a0965545b7
Kewin Brand Robetti <kewin.robetti@viasoft.com.br>
2018-10-07 22:19:22 -0300

    refactor user api to use new mongodb access model

--- src/apis/user.api.ts
+++ src/apis/user.api.ts
@@ -7,33 +7,55 @@ import { isNullOrUndefined } from "util";
 export class User extends BaseRoute{
     
     public getUser(req: Request, res: Response, next: NextFunction){
-        res.locals.mongoAccess.coll[0].find({"email": req.query.email}).toArray(function(err, user_list) {
+
+        this.getMongoAccess(res)
+      .users()
+      .subscribe(userCollection => {
+        userCollection.find({"email": req.query.email}).toArray(function(err, user_list) {
             if(user_list && user_list.length > 0 ) res.send(user_list[0]);
             else res.status(200).send();
-        });   
+        }); 
+
+      });
+
+        
     }
     
     public updateUser(req: Request, res: Response, next: NextFunction){
         let user = req.body as UserModel;
-        res.locals.mongoAccess.coll[0].find({"_id": mongoose.Types.ObjectId(user._id)}).toArray(function(err, user_list) {
+
+        this.getMongoAccess(res)
+      .users()
+      .subscribe(userCollection => {
+        userCollection.find({"_id": mongoose.Types.ObjectId(user._id)}).toArray(function(err, user_list) {
             if(user_list){
-                res.locals.mongoAccess.coll[0].update({"_id": mongoose.Types.ObjectId(user._id)}, {"fullName": user.fullName, "email": user.email, "password": user.password, "picture": user.picture }, function(error, result) {
+                userCollection.update({"_id": mongoose.Types.ObjectId(user._id)}, {"fullName": user.fullName, "email": user.email, "password": user.password, "picture": user.picture }, function(error, result) {
                     res.status(200).send("");
                 });
             }
-        }); 
+        });
+      });
+
+         
     }
     public updateUserPassword(req: Request, res: Response, next: NextFunction){
         let userEmail = req.body.email;
         let newPassword = req.body.newPassword;
-        res.locals.mongoAccess.coll[0].find({"email": userEmail}).toArray(function(err, user_list) {
+
+        this.getMongoAccess(res)
+      .users()
+      .subscribe(userCollection => {
+        userCollection.find({"email": userEmail}).toArray(function(err, user_list) {
             if(user_list){
-                res.locals.mongoAccess.coll[0].update({"email": userEmail}, { $set: {"password": newPassword }});
+                userCollection.update({"email": userEmail}, { $set: {"password": newPassword }});
                 res.status(200).json({ message: 'updated' });
             }
             else{
                 res.status(200).json({ message: 'not_updated' });
             }
-        }); 
+        });
+      });
+
+         
     }
 } 
