ce1ece8402edab5b7b7ab182a2f203844b02129b
Kewin Brand Robetti <kewin.robetti@viasoft.com.br>
2018-10-07 22:11:01 -0300

    code cleanup

--- src/apis/tec_compart_nota.ts
+++ src/apis/tec_compart_nota.ts
@@ -1,53 +1,51 @@
-import { UserModel } from '../models/user.model';
-import { BaseRoute } from '../routes/route';
+import { UserModel } from "../models/user.model";
+import { BaseRoute } from "../routes/route";
 import { NextFunction, Request, Response } from "express";
-import * as mongoose from 'mongoose'
-import { TecladoCompartilhadoModel } from '../models/teclado_compartilhado.model';
-import { TecladoCompartilhadoNotaModel } from '../models/teclado_compartilhado.nota.model';
+import * as mongoose from "mongoose";
+import { TecladoCompartilhadoModel } from "../models/teclado_compartilhado.model";
+import { TecladoCompartilhadoNotaModel } from "../models/teclado_compartilhado.nota.model";
 // import * as mongoSanitize from 'express-mongo-sanitize';
 
-export class TecladoCompartihadoNota extends BaseRoute{
-    
-    public getNotaMedia(req: Request, res: Response, next: NextFunction){
-
-        this.getMongoAccess(res)
+export class TecladoCompartihadoNota extends BaseRoute {
+  public getNotaMedia(req: Request, res: Response, next: NextFunction) {
+    this.getMongoAccess(res)
       .tec_compart_notas()
       .subscribe(tecNotasCollection => {
-
-        tecNotasCollection.find({"teclado": req.query.teclado}).toArray(function(err, lista_notas) {
+        tecNotasCollection
+          .find({ teclado: req.query.teclado })
+          .toArray(function(err, lista_notas) {
             let quant_rest = lista_notas.length;
-            let media :number = 0;
-            for(let i of lista_notas){
-                media = media + Number.parseInt(i.nota);
+            let media: number = 0;
+            for (let i of lista_notas) {
+              media = media + Number.parseInt(i.nota);
             }
             media = media / quant_rest;
-            res.send({value: media,numero_aval: quant_rest});
-        }); 
-
+            res.send({ value: media, numero_aval: quant_rest });
+          });
       });
+  }
 
-        
-    }
-    
-    public getJaAvaliou(req: Request, res: Response, next: NextFunction){
-
-        this.getMongoAccess(res)
-        .tec_compart_notas()
-        .subscribe(tecNotasCollection => {
-
-            tecNotasCollection.find({"teclado": req.query.teclado,"usuarioEmail": req.query.usuarioEmail}).toArray(function(err, resposta) {
-                if(resposta.length >= 1){
-                    res.send({value: true})
-                }
-                else{
-                    res.locals.mongoAccess.coll[5].insert(
-                        {"teclado": req.query.teclado,"usuarioEmail": req.query.usuarioEmail,"nota": req.query.nota}
-                    );
-                    res.send({value: false})
-                }
-            });   
-        });
-
-        
-    }
-} 
+  public getJaAvaliou(req: Request, res: Response, next: NextFunction) {
+    this.getMongoAccess(res)
+      .tec_compart_notas()
+      .subscribe(tecNotasCollection => {
+        tecNotasCollection
+          .find({
+            teclado: req.query.teclado,
+            usuarioEmail: req.query.usuarioEmail
+          })
+          .toArray(function(err, resposta) {
+            if (resposta.length >= 1) {
+              res.send({ value: true });
+            } else {
+              res.locals.mongoAccess.coll[5].insert({
+                teclado: req.query.teclado,
+                usuarioEmail: req.query.usuarioEmail,
+                nota: req.query.nota
+              });
+              res.send({ value: false });
+            }
+          });
+      });
+  }
+}
