406f45b65b027f2d697d6ee4609c91b7dbd8813b
Kewin Brand Robetti <kewin.robetti@viasoft.com.br>
2018-10-07 21:57:59 -0300

    code cleanup

--- src/apis/auth.api.ts
+++ src/apis/auth.api.ts
@@ -1,24 +1,17 @@
-import { NextFunction, Request, Response, Router } from "express";
+import { Request } from "express";
 import { BaseRoute } from "../routes/route";
-import { KeyboardModel } from '../models/keyboard.model';
-import { MongoAccessModel } from "../models/mongoAccess.model";
-import * as CryptoJS from 'crypto-js';
-import * as jwt from 'jsonwebtoken';
-import { backendConfig } from '../backend.config';
 
-
-export class Auth extends BaseRoute{
-
-    public extractToken(req: Request): string {
-        let token = undefined;
-        let parts: string[] = [];
-        if (req.headers && req.headers.authorization) {
-            //Authorization: Bearer ZZZ.ZZZ.ZZZ
-            parts = req.headers.authorization.toString().split(' ')
-            if (parts.length === 2 && parts[0] === 'Bearer') {
-                token = parts[1]
-            }
-        }
-        return token
+export class Auth extends BaseRoute {
+  public extractToken(req: Request): string {
+    let token = undefined;
+    let parts: string[] = [];
+    if (req.headers && req.headers.authorization) {
+      //Authorization: Bearer ZZZ.ZZZ.ZZZ
+      parts = req.headers.authorization.toString().split(" ");
+      if (parts.length === 2 && parts[0] === "Bearer") {
+        token = parts[1];
+      }
     }
+    return token;
+  }
 }
