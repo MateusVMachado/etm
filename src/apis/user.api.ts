9a35c73c664b73c2f943334d8746a4f648cf525f
Kewin Brand Robetti <kewin.robetti@viasoft.com.br>
2018-10-07 22:19:32 -0300

    code cleanup

--- src/apis/user.api.ts
+++ src/apis/user.api.ts
@@ -1,61 +1,69 @@
 import { NextFunction, Request, Response } from "express";
-import * as mongoose from 'mongoose';
-import { UserModel } from '../models/user.model';
-import { BaseRoute } from '../routes/route';
+import * as mongoose from "mongoose";
+import { UserModel } from "../models/user.model";
+import { BaseRoute } from "../routes/route";
 import { isNullOrUndefined } from "util";
 
-export class User extends BaseRoute{
-    
-    public getUser(req: Request, res: Response, next: NextFunction){
-
-        this.getMongoAccess(res)
+export class User extends BaseRoute {
+  public getUser(req: Request, res: Response, next: NextFunction) {
+    this.getMongoAccess(res)
       .users()
       .subscribe(userCollection => {
-        userCollection.find({"email": req.query.email}).toArray(function(err, user_list) {
-            if(user_list && user_list.length > 0 ) res.send(user_list[0]);
+        userCollection
+          .find({ email: req.query.email })
+          .toArray(function(err, user_list) {
+            if (user_list && user_list.length > 0) res.send(user_list[0]);
             else res.status(200).send();
-        }); 
-
+          });
       });
+  }
 
-        
-    }
-    
-    public updateUser(req: Request, res: Response, next: NextFunction){
-        let user = req.body as UserModel;
+  public updateUser(req: Request, res: Response, next: NextFunction) {
+    let user = req.body as UserModel;
 
-        this.getMongoAccess(res)
+    this.getMongoAccess(res)
       .users()
       .subscribe(userCollection => {
-        userCollection.find({"_id": mongoose.Types.ObjectId(user._id)}).toArray(function(err, user_list) {
-            if(user_list){
-                userCollection.update({"_id": mongoose.Types.ObjectId(user._id)}, {"fullName": user.fullName, "email": user.email, "password": user.password, "picture": user.picture }, function(error, result) {
-                    res.status(200).send("");
-                });
+        userCollection
+          .find({ _id: mongoose.Types.ObjectId(user._id) })
+          .toArray(function(err, user_list) {
+            if (user_list) {
+              userCollection.update(
+                { _id: mongoose.Types.ObjectId(user._id) },
+                {
+                  fullName: user.fullName,
+                  email: user.email,
+                  password: user.password,
+                  picture: user.picture
+                },
+                function(error, result) {
+                  res.status(200).send("");
+                }
+              );
             }
-        });
+          });
       });
+  }
+  public updateUserPassword(req: Request, res: Response, next: NextFunction) {
+    let userEmail = req.body.email;
+    let newPassword = req.body.newPassword;
 
-         
-    }
-    public updateUserPassword(req: Request, res: Response, next: NextFunction){
-        let userEmail = req.body.email;
-        let newPassword = req.body.newPassword;
-
-        this.getMongoAccess(res)
+    this.getMongoAccess(res)
       .users()
       .subscribe(userCollection => {
-        userCollection.find({"email": userEmail}).toArray(function(err, user_list) {
-            if(user_list){
-                userCollection.update({"email": userEmail}, { $set: {"password": newPassword }});
-                res.status(200).json({ message: 'updated' });
+        userCollection
+          .find({ email: userEmail })
+          .toArray(function(err, user_list) {
+            if (user_list) {
+              userCollection.update(
+                { email: userEmail },
+                { $set: { password: newPassword } }
+              );
+              res.status(200).json({ message: "updated" });
+            } else {
+              res.status(200).json({ message: "not_updated" });
             }
-            else{
-                res.status(200).json({ message: 'not_updated' });
-            }
-        });
+          });
       });
-
-         
-    }
-} 
+  }
+}
