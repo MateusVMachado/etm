9c9bfe300d65f1fd907dc5bede48e35109abbdcc
Gabriel Branco <gabriel.azevedo@viasoft.com.br>
2019-01-22 08:36:00 -0200

    stora-shared module

--- src/app/components/store/store.module.ts
+++ src/app/components/store/store.module.ts
@@ -8,27 +8,27 @@ import { StoreItemComponent } from '../storeItem/storeItem.component';
 import { TecladoBaseModule } from '../teclado-base/teclado-base.module';
 import { StoreComponent } from './store.component';
 import { storeRoute } from './store.routing';
+import { StoreSharedModule } from '../store-shared/store-shared.module';
 
 
 @NgModule({
-    imports: [
-        CommonModule,
-        FormsModule,
-        ReactiveFormsModule,
-        TecladoBaseModule,
-        NgxPaginationModule,
-        RouterModule.forChild(storeRoute)        
-    ],
-    declarations: [
-        StoreComponent,
-        StoreItemComponent,
-    ],
-    exports: [
-        StoreComponent
-    ],
-    providers: [TecladoCompartilhadoService],
-    schemas: [
-        CUSTOM_ELEMENTS_SCHEMA
-    ]
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    CommonModule,
+    NgxPaginationModule,
+    RouterModule.forChild(storeRoute),
+    StoreSharedModule      
+  ],
+  declarations: [
+    StoreComponent,
+  ],
+  exports: [
+    StoreComponent
+  ],
+  providers: [TecladoCompartilhadoService],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ]
 })
 export class StoreModule { }
--- src/app/components/storeItem/storeItem.component.module.ts
+++ src/app/components/storeItem/storeItem.component.module.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { TecladoBaseModule } from '../teclado-base/teclado-base.module';
 import { StoreItemComponent } from './storeItem.component';
+import { StoreSharedModule } from '../store-shared/store-shared.module';
 @NgModule({
-    declarations: [ StoreItemComponent ],
-    imports: [ 
-        CommonModule,
-        TecladoBaseModule
+    declarations: [  ],
+    imports: [
+      StoreSharedModule,
+      CommonModule,
     ]
 })
 export class StoreItemModule {}
