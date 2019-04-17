a7ba3fd73ab64136f6381c1632079d961bb54ea3
Mateus Vieira <mateus.machado@viasoft.com.br>
2019-04-17 17:09:04 -0300

    ETM-87 Mateus Vieira
    - Adicionado log de tecla pressionada junto do horário;

--- src/models/userSession.model.ts
+++ src/models/userSession.model.ts
@@ -3,17 +3,26 @@ export class TimeIntervalUnit {
     outTime: string;
 }
 
+export class KeyPressedAt {
+    key: string;
+    time: string;
+    scanTimeOfLines: string;
+    scanTimeOfColumns: string;
+  }
+
 export class UserSessionModel {
     
 
     ip: string;
     user: string;
+    lastKeyboard: string;
     access: string;
     logout: string;
     sessDate: string;
     sessDuration: string;
     configIntervals: Array<TimeIntervalUnit>;
     configUsage: string;
+    keyPressedAt: Array<KeyPressedAt>;
     keyboardIntervals: Array<TimeIntervalUnit>;
     keyboardUsage: string;
     layoutEditorIntervals: Array<TimeIntervalUnit>;
